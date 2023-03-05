aba4df7691a5031968c6adfecac44cf5b2b5c6d9
shadowmouse89 <108416539+shadowmouse89@users.noreply.github.com>
2023-03-05 03:43:23 +0000

    HW 5 DNA

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,73 @@
 //add include statements
+#include "func.h"
+
+
+
+
 
 //add function code here
+double get_gc_content(const string& dna) 
+{
+    int gcCount = 0;
+
+    for(int i = 0; i < dna.size(); i++) 
+    {
+        if(dna[i] == 'G' || dna[i] == 'C')
+        {
+            gcCount++;
+        }
+        
+    }
+
+    double gcPercent = static_cast<double>(gcCount) / dna.size();
+    cout<<gcPercent<<"\n";
+    
+    return gcPercent;
+}
+
+string reverse_string(string dna)
+{
+    string rDNA;
+
+    for(int i = dna.length() - 1; i >= 0; i--)
+    {
+        rDNA += dna[i];
+    }
+    
+    return rDNA;
+}
+
+
+
+   string get_dna_complement(string dna)
+{
+    string cDNA = reverse_string(dna);
+
+    int dna_comp_length = cDNA.length();
+
+	for (int i = 0; i < dna_comp_length; i++)
+		if (cDNA[i] == 'A')
+		{
+            cDNA[i] = 'T';
+            
+        }	
+        else if (cDNA[i] == 'T')
+		{
+            cDNA[i] = 'A';
+            
+        }	
+        else if (cDNA[i] == 'C')
+		{
+            cDNA[i] = 'G';
+            
+        }	
+        else if (cDNA[i] == 'G')
+		{
+            cDNA[i] = 'C';
+            
+        }	
+
+    return cDNA;
+}
+
+    
